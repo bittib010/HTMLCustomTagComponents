639a33b017e109c0630c9a2e3a328c7a83e28566
Adrian Kydland Skaftun <77796831+bittib010@users.noreply.github.com>
2025-02-08 12:35:23 +0100

    width listener added and style updates

--- hexviewer/hexviewer.js
+++ hexviewer/hexviewer.js
@@ -15,6 +15,7 @@ class AsciiHexElement extends HTMLElement {
     this.attachListeners();
     this.row_col_listener();
     this.changeOffsetListener();
+    this.changeWidthListener();
   }
 
   attachListeners() {
@@ -65,7 +66,23 @@ class AsciiHexElement extends HTMLElement {
       }
     });
   }
- 
+
+  changeWidthListener() {
+    const widthInput = this.querySelector('#hex-width');
+    if (widthInput) {
+      widthInput.addEventListener('input', (event) => {
+        const newWidth = parseInt(event.target.value);
+        if (!isNaN(newWidth) && newWidth > 0) {
+          this.lineshift = newWidth;
+          this.innerHTML = this.render();
+          this.attachListeners();
+          this.row_col_listener();
+          this.changeOffsetListener();
+          this.changeWidthListener();
+        }
+      });
+    }
+  }
 
   render() {
     this.hexNums = this.hexNums.split(" ");
